849093d01e008d6337b782c6b531b011fb85d97f
gostrider <tiddybeardaywalker@hotmail.com>
2020-12-17 12:22:05 +0800

    modify store structure with schema

--- src/DataStore.hs
+++ src/DataStore.hs
@@ -2,31 +2,40 @@
 
 module DataStore where
 
-import           Data.Char (isDigit)
-import           Data.List (findIndex, isInfixOf)
-import           Data.Text (Text, pack, strip, toLower, unpack)
+import           Data.Char (isDigit, isLetter, isSpace)
+import           Data.List (findIndex, isInfixOf, map)
+import           Data.Text (Text, pack, split, strip, toLower, unpack)
 
 
-data DataStore = MkStore { size :: Int, items :: [String] } deriving Show
+data DataStore = MkStore { size :: Int, items :: [Schema] } deriving Show
 
 
-data Command = Add String
-             | Get Int
-             | Size
-             | Search String
-             | Quit
-             deriving Show
+data Command
+  = Add Schema
+  | Get Int
+  | Size
+  | Search Schema
+  | Quit
+  deriving Show
+
+
+data Schema
+  = SInt Int
+  | SString String
+  | Pair Schema Schema
+  | Unit -- should we have this case?
+  deriving (Show, Eq)
 
 
 storeSize :: DataStore -> Int
 storeSize store = size store
 
 
-storeItems :: DataStore -> [String]
+storeItems :: DataStore -> [Schema]
 storeItems store = items store
 
 
-addItems :: DataStore -> String -> DataStore
+addItems :: DataStore -> Schema -> DataStore
 addItems store item = MkStore {size = size', items = items'}
   where
     size' = storeSize store + 1
@@ -37,7 +46,7 @@ initStore :: DataStore
 initStore = MkStore { size = 0, items = [] }
 
 
-getItem :: Int -> DataStore -> Maybe (String, DataStore)
+getItem :: Int -> DataStore -> Maybe (Schema, DataStore)
 getItem pos store =
   let
     lastIndex = storeSize store
@@ -48,24 +57,32 @@ getItem pos store =
     else Nothing
 
 
-searchItem :: DataStore -> String -> Maybe (String, DataStore)
+searchItem :: DataStore -> Schema -> Maybe (Schema, DataStore)
 searchItem store target =
-  let
-    items = storeItems store
-    idx = findIndex (isInfixOf target) items
-  in
-    case idx of
-      Just i  -> getItem i store
-      Nothing -> Nothing
+  case findIndex (== target) (storeItems store) of
+    Just i  -> getItem i store
+    Nothing -> Nothing
+
+
+parseSchema :: [String] -> Schema
+parseSchema [] = Unit
+parseSchema [x] | x == ""           = Unit
+                | isLetter (head x) = SString x
+                | isDigit (head x)  = SInt (read x :: Int)
+parseSchema (x : xs) = Pair (parseSchema [x]) (parseSchema xs)
+
+
+splitArgs :: String -> [String]
+splitArgs args = map unpack $ split isSpace $ pack args
 
 
 parseCommand :: String -> String -> Maybe Command
-parseCommand "add" item   = Just $ Add item
-parseCommand "get" key    = if all isDigit key then Just (Get (read key :: Int)) else Nothing
-parseCommand "size" ""    = Just Size
-parseCommand "search" str = Just $ Search str
-parseCommand "quit" ""    = Just Quit
-parseCommand _ _          = Nothing
+parseCommand "add" item    = Just . Add . parseSchema . splitArgs $ item
+parseCommand "get" key     = if all isDigit key then Just (Get (read key :: Int)) else Nothing
+parseCommand "size" ""     = Just Size
+parseCommand "search" item = Just . Search . parseSchema . splitArgs $ item
+parseCommand "quit" ""     = Just Quit
+parseCommand _ _           = Nothing
 
 
 parse :: String -> Maybe Command
@@ -78,15 +95,28 @@ parse input =
       (cmd, args) -> parseCommand (formatStr toLower cmd) (formatStr strip args)
 
 
+display :: Schema -> String
+display Unit         = ""
+display (SInt x)     = show x
+display (SString x)  = x
+display (Pair s1 s2) = display s1 ++ " " ++ display s2
+
+
 processInput :: DataStore -> String -> Maybe (String, DataStore)
 processInput store input =
   case parse input of
-    Just (Add item)   -> Just ("ID: " ++ (show $ storeSize store), addItems store item)
+    Just (Add item)   -> Just ("ID: " ++ (show . storeSize $ store), addItems store item)
+
     Just (Get key)    -> case (getItem key store) of
-                           Just (i, store') -> Just (show i, store')
+                           Just (i, store') -> Just (display i, store')
                            Nothing -> Just ("Failed to get item", store)
+
     Just Size         -> Just (show $ storeSize store, store)
-    Just (Search str) -> searchItem store str
+
+    Just (Search schema) -> case (searchItem store schema) of
+                              Just (i, store') -> Just (display i, store')
+                              Nothing -> Just ("Item not found", store)
+
     Just Quit         -> Nothing
     Nothing           -> Just ("Invalid command", store)
 
